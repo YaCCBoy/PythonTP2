e6bba9d39aff1f67afaf2b55dce584bd72cba900
Cedric Chagnon <lepepitte@protonmail.com>
2023-05-03 10:50:20 -0400

    Getter et setter pour chalet

--- serveur/super_chalets_serveur.py
+++ serveur/super_chalets_serveur.py
@@ -3,10 +3,52 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class Chalet:
       def __init__(self, chalet_id, nom, url_image, latitude, longitude, plages):
-            self.__id = chalet_id
+            self.__chalet_id = chalet_id
             self.__nom = nom
             self.__url_image = url_image
             self.__geolocalisation = [latitude, longitude]
             self.__plages = plages
+
+      @property
+      def chalet_id(self):
+            return self.__chalet_id
+
+      @chalet_id.setter
+      def chalet_id(self, valeur):
+            self.__chalet_id = valeur
+
+      @property
+      def nom(self):
+            return self.__nom
+
+      @nom.setter
+      def nom(self, valeur):
+            self.__nom = valeur
+
+      @property
+      def url_image(self):
+            return self.__url_image
+
+      @url_image.setter
+      def url_image(self, valeur):
+            self.__url_image = valeur
+
+      @property
+      def geolocalisation(self):
+            return self.__geolocalisation
+
+      @geolocalisation.setter
+      def geolocalisation(self, latitude, longitude):
+            self.__geolocalisation = []
+            self.__geolocalisation.append(latitude)
+            self.__geolocalisation.append(longitude)
+
+      @property
+      def plages(self):
+            return self.__plages
+
+      @plages.setter
+      def plages(self, valeur):
+            self.__plages = valeur
 
 class Utilisateur:
