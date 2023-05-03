87ad7a4afcda4fe0c7471bed8006e7e36baaa184
Tom <thomas.langdeau@gmail.com>
2023-05-03 10:10:06 -0400

    Enlever la classe merdeuse

--- serveur/super_chalets_serveur.py
+++ serveur/super_chalets_serveur.py
@@ -1,24 +1,3 @@
 import json
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
-class merdeuse() : 
-
-    def __init__(self, excrements, jaune) :
-            self.excrements = excrements
-            self.jaune = jaune
-
-    def safourien() : 
-          pass
-    
-    def unautresafourien() : 
-          pass
-    
-    def __repr__(self) : 
-          return self.excrements + " sont jaunes " + self.jaune
-     
-print("C<est de la merde ce truc")
-
-crotte = merdeuse("truc brun", "ce n'est pas jaune !")
-print(crotte)
-
-#En effet mon cher c'est de la grosse merde
