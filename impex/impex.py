d5d39edbd1eedf3f1e2db7304f8e72ac58b25c70
Tom <thomas.langdeau@gmail.com>
2023-05-08 15:04:15 -0400

    j'pense que l'ouverture des différents fichiers fonctionne

--- impex/impex.py
+++ impex/impex.py
@@ -1,11 +1,23 @@
 import csv
 import xmltodict
 
-with open("./data/utilisateurs.csv") as csvfile :
-    reader1 = csv.DictReader(csvfile)
+def data_utilisateurs() :
+    with open("./data/utilisateurs.csv") as csvfile :
+        reader1 = csv.DictReader(csvfile)
 
-with open('./data/chalets.csv') as csvfile :
-    reader2 = csv.DictReader(csvfile)
 
-with open('./data/reservations.xml', 'r') :
-    xml_string = file.read()
+def data_chalets() :
+    with open('./data/chalets.csv') as csvfile :
+        reader2 = csv.DictReader(csvfile)
+
+def data_reservations() :
+    file1 = open('./data/reservations.xml', 'r')
+    xml_string = file1.read()
+    reservation_dict = xmltodict.parse(xml_string)
+    print(reservation_dict)
+
+def data_dipos() :
+    file2 = open('./data/disponibilites.xml', 'r')
+    xml_string = file2.read()
+    dispos_dict = xmltodict.parse(xml_string)
+    print(dispos_dict)
