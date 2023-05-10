d3bcdead92a153a2b84c6750f33cb546d41b82f1
Tom <thomas.langdeau@gmail.com>
2023-05-10 11:04:59 -0400

    continuemle client

--- client/client.py
+++ client/client.py
@@ -11,16 +11,27 @@ class ClientServeurChalet:
         print(req.status_code)
         print(req.content)
 
-    def get_reservations_utilisateur(self):
-        pass
-    def post_reservation(self):
-        pass
-    def put_reservation(self):
-        pass
+    def get_reservations_utilisateur(self, reservations_c):
+        req = requests.get(self.__url_base + '/reservation du client/' + reservations_c)
+        print(req.status_code)
+        print(req.content)
+    def post_reservation(self, reservation):
+        json_body = '{"reservation": "' + reservation + '"}'
+        req = requests.post(self.__url_base + '/reservation', data=json_body)
+        print(req.status_code)
+        print(req.content)
+    def put_reservation(self, data_reservations, reservation):
+        json_body = '{"reservation": "' + reservation + '" }'
+        req = requests.post(self.__url_base + '/data_reservations/' + data_reservations, data=json_body)
+        print(req.status_code)
+        print(req.content)
     def delete_reservation(self):
         pass
-    def post_utilisateur(self):
-        pass
+    def post_utilisateur(self, utilisateur):
+        json_body = '{"utilisateur": "' + utilisateur + '"}'
+        req = requests.post(self.__url_base + '/utilisateur', data=json_body)
+        print(req.status_code)
+        print(req.content)
     def get_reservations_liste(self):
         pass
     def post_chalet(self):
